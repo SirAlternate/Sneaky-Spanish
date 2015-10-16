458f1aa8ee981f7d80da54031c478e22d19458fb
Erik Saulnier <info@eriksaulnier.com>
2015-10-16 18:00:05 -0400

    Add: Outline of extension with simple replacing of words

---
+++ js/content.js
@@ -0,0 +1,20 @@
+// Runs the onLoad function every time a page is loaded
+onLoad();
+
+function onLoad() {
+  // List of words to replace - TODO: Should probably be in a data file
+  var wordsToReplace = [
+    ["the ", "el "],
+    ["The ", "El "]
+  ];
+
+  // Goes through the list of words to replace and replaces them on the page
+  for (var i = 0; i < wordsToReplace.length; i++) {
+    replaceWord(wordsToReplace[i][0], wordsToReplace[i][1]);
+  }
+}
+
+// Function for replacing given words (word1 is the word to be replaced)
+function replaceWord(word1, word2) {
+  document.body.innerHTML = document.body.innerHTML.replace(new RegExp(word1, "g"), word2);
+}
