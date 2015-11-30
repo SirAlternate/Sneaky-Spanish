b42a43f31d557d457e938e386c39889628b2d8dc
austindoeswork <wilsonaustin@gmail.com>
2015-11-30 14:51:35 -0500

    added api call translation

--- js/content.js
+++ js/content.js
@@ -12,28 +12,39 @@ function onLoad(localStatus) {
     ["giraffe", "jirafa"],
     ["Giraffe", "Jirafa"]
   ];
+  var words = ["giraffe", "Giraffe", "species", "tallest", "living"];
 
   // only replace words if status is 1 (SS enabled)
   if (localStatus == 1) {
     
     // get the document body
-    var body = document.body;
-    if(body == null) { // just incase
-      console.error("ERR: document has no body");
-      return;
-    }
+    // var body = document.body;
+    // if(body == null) { // just incase
+    //   console.error("ERR: document has no body");
+    //   return;
+    // }
     
+    //HANDLEING WORDS IN FOR LOOP
+    for (var i = 0; i < words.length; i++) {
+      handle(words[i]);
+    }
+
     // goes through the list of words to replace and replaces them on the page
-    for (var i = 0; i < wordsToReplace.length; i++) {
-      replaceWord(wordsToReplace[i][0], wordsToReplace[i][1], body);
-    } 
+    // for (var i = 0; i < wordsToReplace.length; i++) {
+    //   replaceWord(wordsToReplace[i][0], wordsToReplace[i][1]);
+    // } 
     
     // TODO => add functionality to replaced words (should move this to another doc)
     // get a list of all changed words in the document
-    var elements = document.getElementsByClassName("sneakyWord");
+  }
+}
+
+function addJ(className){ 
+    var elements = document.getElementsByClassName(className);
     for(var i=0; i<elements.length; i++){ 
       // add click event
       elements[i].onmousedown = function(event) {
+        console.log("quit it");
         if (event.which == 3) {  
           // hide already-toggled popovers
           $('.sneakyWord').popover('hide');
@@ -45,16 +56,12 @@ function onLoad(localStatus) {
       // get rid of stupid menu thing
       elements[i].setAttribute("onContextMenu","return false;");
     }
-    
     // body click event for hiding popovers
     $('body').on('click', function(e) {
       if($(e.target).data('toggle') !== 'popover') {
         $('[data-toggle="popover"]').popover('hide');
       }
     })
-
-  }
-
 }
 
 function populatePopover(element) {
@@ -65,11 +72,11 @@ function populatePopover(element) {
 }
 
 // Function for replacing given words (word1 is the word to be replaced)
-function replaceWord(word1, word2, body) {
-  console.log("replacing: " + word1);            //!//start replacing
+function replaceWord(word1, word2) {
+  console.log("replacingg: " + word1);            //!//start replacing
 
   var element = document.createElement('em');     //create an element to embed
-  element.className='sneakyWord';
+  element.className="ss" + word1;
   if (false) {
     $(element).attr('style', 'background-color:yellow; color:black;');
   }
@@ -81,6 +88,7 @@ function replaceWord(word1, word2, body) {
   var qString = "\\b" + word1 + "\\b";              //'//b' is to omit embedded words (like rather and other for the)
   var findMe = new RegExp(qString, "g");            //make regex
 
+  var body = document.body;
   findAndReplaceDOMText(body, {                     //replace
     // preset: 'prose',
     find: findMe,
@@ -89,6 +97,29 @@ function replaceWord(word1, word2, body) {
     // }
     replace: word2,
     wrap: element
+  });
+  addJ(word1);
+}
+
+function handle(EWord){
+  translateFromAPI(EWord, function(SWord){
+    replaceWord(EWord,SWord);
+  });
+}
+function translateFromAPI(EWord, cb){
+  console.log("translating: " + EWord); 
+  $.ajax({
+    type: "GET",
+    url: "https://glosbe.com/gapi/translate?from=eng&dest=spa&format=json&phrase="+EWord,
+    dataType: "json",
+    success: function(responseData, status){
+      console.log("\ttranslated: " + responseData.tuc[0].phrase.text + "\tstatus: " + status);
+      cb(responseData.tuc[0].phrase.text);
+    },
+    error: function(msg) {
+      // there was a problem
+      alert("There was a problem: " + msg.status + " " + msg.statusText);
+    }
   });
 }
 
