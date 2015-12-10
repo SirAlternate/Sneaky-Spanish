d48ccbff7e9ba7a5b79af50eadbd6df2ffcc4372
austindoeswork <wilsonaustin17@gmail.com>
2015-12-09 20:06:34 -0500

    refresh on switch

--- js/eventPage.js
+++ js/eventPage.js
@@ -7,6 +7,10 @@ chrome.runtime.onMessage.addListener(
       sendResponse({status: localStorage['status']});
     }
     if(request.method == "changeIcon") {
+      chrome.tabs.getSelected(null, function(tab) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(tab.id, {code: code});
+      });
     	chrome.browserAction.setIcon({
             path: request.path,
             // tabId: sender.tab.id
